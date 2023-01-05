3058ecd136fa3a7d14c7b1556092551f7ba6f3ef
Sachin Gande <sachin.gande@BRILLIO.COM>
2023-01-05 12:37:25 +0530

    initial commit

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Copyright &copy; ZomatoClone</div>
+  );
+};
+
+export default Footer;
